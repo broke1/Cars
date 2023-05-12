b2006b108af9b52ad6d038b5cdf926fe20a97423
broke1 <fredswork@list.ru>
2023-05-12 13:41:18 +0300

    start work

---
+++ src/store/store.js
@@ -0,0 +1,16 @@
+export default {
+    state: {
+        name: 'Cars'
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    },
+    getters: {
+        getName(state) {
+            return state.name
+        }
+    }
+}
